2ce90ef324d693e885e93b9ce35dec34b30148b0
Zhiping Shu <ZShu@slb.com>
2023-11-02 17:19:19 +0800

    feat: add the implement of personal self Introduce.

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOStepByStep
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        public Person(string name, int age) 
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public string SelfIntroduceMsg()
+        {
+            return $"My name is {name}. I am {age} years old.";
+        }
+    }
+}
--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -11,5 +11,20 @@ namespace OOStepByStepTest
             var class1 = new Class1();
             Assert.NotNull(class1);
         }
+
+        [Theory]
+        [InlineData("Tom", 21)]
+        [InlineData("Amy", 30)]
+        public void Should_return_personIntroMsg_given_personInfo(string name, int age)
+        {
+            //given
+            Person person = new Person(name, age);
+
+            //when
+            string msg = person.SelfIntroduceMsg();
+
+            //then
+            Assert.Equal($"My name is {name}. I am {age} years old.", msg);
+        }
     }
 }
