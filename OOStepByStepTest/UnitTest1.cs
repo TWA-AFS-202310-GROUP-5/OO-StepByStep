f712a33e95f18c4cd3c682a1757dfee117adb64f
Yang Liu <YLiu148@slb.com>
2023-11-02 17:15:38 +0800

    feat: implement AC1--person self-introduction

--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -1,15 +1,26 @@
 namespace OOStepByStepTest
 {
     using OOStepByStep;
+    using System.Xml.Linq;
     using Xunit;
 
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Should_return_person_introduction_when_introduce_given_person()
         {
-            var class1 = new Class1();
-            Assert.NotNull(class1);
+            //Given
+            var name = "Tom";
+            var age = 10;
+
+            //When
+            var person = new Person { Name = name, Age = age };
+            var result = person.Introduce();
+
+            //Then
+            Assert.Equal($"I am {name}, I am {age}years old", result);
+            
+
         }
     }
 }
