4f2fa00f36492347a40a69d164eedf1cd5494619
Jinglin Wang <JWang317@slb.com>
2023-11-02 16:35:17 +0800

    test: add ut for a [Person] should introduce his/her name and age

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOStepByStep
+{
+    public class Person
+    {
+        public Person(string n, int a)
+        {
+            Name = n;
+            Age = a;
+        }
+
+        private string Name { get; set; }
+        private int Age { get; set; }
+
+        public object Introduce()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -11,5 +11,18 @@ namespace OOStepByStepTest
             var class1 = new Class1();
             Assert.NotNull(class1);
         }
+
+        [Theory]
+        [InlineData("Alice", 10)]
+        [InlineData("Jack", 15)]
+        [InlineData("Bob", 13)]
+        public void Should_return_self_introduction_given_person_with_name_and_age(string name, int age)
+        {
+            var person = new Person(name, age);
+
+            var result = person.Introduce();
+
+            Assert.Equal(result, $"My name is {name}. I am {age} years old.");
+        }
     }
 }
