1a7fec5e005a6f870aaff7a23036f9157993a162
Xin <xzhao34@slb.com>
2023-11-02 16:51:42 +0800

    feat: create class named Class

---
+++ OOStepByStep/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOStepByStep
+{
+    public class Class
+    {
+        private Teacher teacher;
+        private List<Student> students = new List<Student>();
+    }
+}
