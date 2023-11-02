e071c28a3e2a61a90a019c0e1fa975bf2f4b262c
Jinglin Wang <JWang317@slb.com>
2023-11-02 17:05:25 +0800

    test: add ut for teacher and student, should return self introduction contains name, age and class

--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -39,9 +39,9 @@ namespace OOStepByStepTest
         }
 
         [Theory]
-        [InlineData("Alice", 10)]
-        [InlineData("Jack", 15)]
-        [InlineData("Bob", 13)]
+        [InlineData("Alice", 30)]
+        [InlineData("Jack", 45)]
+        [InlineData("Bob", 33)]
         public void Should_return_self_introduction_given_teacher_with_name_and_age(string name, int age)
         {
             var teacher = new Teacher(name, age);
@@ -49,6 +49,32 @@ namespace OOStepByStepTest
             var result = teacher.Introduce();
 
             Assert.Equal(result, $"My name is {name}. I am {age} years old. I am a teacher.");
+        }
+
+        [Theory]
+        [InlineData("Alice", 30, 2)]
+        [InlineData("Jack", 25, 1)]
+        [InlineData("Bob", 43, 5)]
+        public void Should_return_self_introduction_given_teacher_with_name_and_age_and_class(string name, int age, int classNumber)
+        {
+            var teacher = new Teacher(name, age);
+
+            var result = teacher.Introduce();
+
+            Assert.Equal(result, $"My name is {name}. I am {age} years old. I am a teacher of class {classNumber}");
+        }
+
+        [Theory]
+        [InlineData("Alice", 10, 2)]
+        [InlineData("Jack", 15, 1)]
+        [InlineData("Bob", 13, 5)]
+        public void Should_return_self_introduction_given_student_with_name_and_age_and_class(string name, int age, int classNumber)
+        {
+            var student = new Student(name, age);
+
+            var result = student.Introduce();
+
+            Assert.Equal(result, $"My name is {name}. I am {age} years old. I am a teacher of class {classNumber}");
         }
     }
 }
